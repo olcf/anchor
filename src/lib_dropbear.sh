d591b7f4bd107616915132c31c9d688f84d06f05
Joseph Voss <vossjm@ornl.gov>
2019-09-20 14:52:03 -0400

    Make dropbear dir

--- src/lib_dropbear.sh
+++ src/lib_dropbear.sh
@@ -19,6 +19,7 @@ dropbear_get_certificate() {
   info "dropbear: Getting certificate for ${HOSTNAME} over SSH connection"
 
   info "dropbear: Starting Dropbear server"
+  mkdir -p /etc/dropbear
   dropbear -F -E -R
 
   info "dropbear: Waiting on /client.cert"
