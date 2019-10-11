6890f79f73c96b5bc04a8a64e18cf1815c1c494d
Joseph Voss <vossjm@ornl.gov>
2019-10-11 14:36:36 -0400

    Make initrd not read input. Just builds most up to date initrd + kernel

--- contrib/initrd_build.sh
+++ contrib/initrd_build.sh
@@ -25,22 +25,11 @@ yum install -y lego dropbear-static buildah-static squashfs-tools-static
 echo -e "Installing dracut modules"
 yum install -y dracut-network anchor-dracut-module
 
-mkdir -p /output/
-
 KERNEL_PATH=/boot/
 KERNEL_PATTERN='vmlinuz-'
+
 # Get the most recent kernel available
 KERNEL=$(find "${KERNEL_PATH}" -name "${KERNEL_PATTERN}*" -printf '%f\n' | sort -nr | head -n 1 | sed "s/${KERNEL_PATTERN}//")
-# Use user option if it is set
-if [ ! -z "${KERNEL_SPECIFIED_VALUE}" ]; then
-  USER_KERNEL=${KERNEL_SPECIFIED_VALUE}
-  if [ ! -a "/${KERNEL_PATH}/${KERNEL}" ]; then
-    echo -e "Kernel requested ${USER_KERNEL} doesn't exist in ${KERNEL_PATH}! Using ${KERNEL}."
-  else
-    KERNEL=$USER_KERNEL
-  fi
-fi
-cp "/${KERNEL_PATH}/${KERNEL_PATTERN}${KERNEL}" /output
 
 echo -e "Probing overlay, squashfs, and loop..."
 modprobe overlay squashfs loop mlx4_core mlx4_en mlx4_ib igb bnx2 i40e ipmi_si ipmi_devintf ipmi_msghandler
@@ -49,10 +38,12 @@ echo "hostonly=no" >> /etc/dracut.conf
 
 # Build initrd
 # Force install drivers, modules we want, and no hostonly to build generic image
+mkdir -p /output/
 echo -e "Making initrd..."
 dracut -f -m 'kernel-modules anchor network base' --strip -v \
   --no-hostonly --force-drivers \
   "overlay squashfs loop mlx4_core mlx4_en mlx4_ib igb bnx2 i40e ipmi_si ipmi_devintf ipmi_msghandler" \
   "/output/initrd-${KERNEL}" "${KERNEL}"
 
+cp "/${KERNEL_PATH}/${KERNEL_PATTERN}${KERNEL}" /output
 chmod 655 "/output/initrd-${KERNEL}"
